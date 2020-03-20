e2fbc7bc829396c1ca08d50440f9f3aa15ad2fb7
ivaneroshkin <ivaneroshk@yandex.ru>
2020-03-20 18:26:25 +0300

    Added BDD tests

---
+++ 04 - tic tac toe/features/steps.js
@@ -0,0 +1,60 @@
+const { Given, When, Then } = require('cucumber');
+const assert = require('assert');
+const {
+  displayFieldAsString,
+  displayStringAsField,
+  getSwitch,
+  getCell,
+  checkMoveResult
+} = require('../utils');
+
+const {
+  eventBusyCell
+} = require('../events');
+
+
+let initialField;
+let playerSwitcher;
+let horizontalCell;
+let verticalCell;
+
+Given('пустое поле', () => {
+  initialField = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+});
+
+Given('ходит игрок {int}', (input) => {
+  playerSwitcher = input;
+});
+
+When('игрок ходит в клетку {int}, {int}', (int, int2) => {
+  const fieldSnapshot = JSON.parse(JSON.stringify(initialField));
+  [verticalCell, horizontalCell] = [int, int2];
+  getCell(verticalCell, horizontalCell, initialField, playerSwitcher);
+  if (JSON.stringify(fieldSnapshot) !== JSON.stringify(initialField)) {
+    playerSwitcher = getSwitch(playerSwitcher);
+  }
+});
+
+Then('поле становится {string}', (string) => {
+  assert.equal(displayFieldAsString(initialField), string);
+});
+
+Given('поле {string}', (string) => {
+  initialField = displayStringAsField(string);
+});
+
+Then('возвращается ошибка', () => {
+  eventBusyCell();
+});
+
+Then('победил игрок {int}', (int) => {
+  let winner = 0;
+  if (checkMoveResult(initialField)) {
+    winner = getSwitch(playerSwitcher);
+  }
+  assert.equal(int, winner);
+});
